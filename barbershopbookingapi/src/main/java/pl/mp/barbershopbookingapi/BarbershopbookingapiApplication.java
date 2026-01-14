d7fcf48e3f95d9e66eefc27852713876703d9272
unknown <mateuszproszowski02@gmail.com>
2026-01-14 11:15:20 +0100

    initial

---
+++ barbershopbookingapi/src/main/java/pl/mp/barbershopbookingapi/BarbershopbookingapiApplication.java
@@ -0,0 +1,13 @@
+package pl.mp.barbershopbookingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbershopbookingapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbershopbookingapiApplication.class, args);
+	}
+
+}
---
+++ barbershopbookingapi/src/test/java/pl/mp/barbershopbookingapi/BarbershopbookingapiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mp.barbershopbookingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbershopbookingapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
